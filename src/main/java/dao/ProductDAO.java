8c788d0d183937bc2a1ba547237d8363575f6f94
Zavis <thezavis@gmail.com>
2023-12-29 15:41:44 +1100

    version 1

---
+++ src/main/java/dao/ProductDAO.java
@@ -0,0 +1,71 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Product;
+import sql.connection.DBConnection;
+
+public class ProductDAO {
+	public List<Product> getLatestProducts() throws SQLException {
+		Connection connection = DBConnection.makeConnection();
+
+		String SQL = "SELECT * FROM product;";
+		Statement stmt = connection.createStatement();
+
+		ResultSet resultSet = stmt.executeQuery(SQL);
+
+		ArrayList<Product> list = new ArrayList<Product>();
+
+		while (resultSet.next()) {
+
+			int id = resultSet.getInt("id");
+			String name = resultSet.getString("name");
+			int price = resultSet.getInt("price");
+			String imgName = resultSet.getString("img_name");
+			boolean isNew = resultSet.getBoolean("is_new");
+			int quantity = resultSet.getInt("quantity");
+			String description = resultSet.getString("description");
+
+			Product product = new Product(id, name, price, imgName, isNew, quantity, description);
+			list.add(product);
+		}
+
+		return list;
+	}
+	
+	public Product getProductById(int productId) throws SQLException {
+		
+		Connection connection = DBConnection.makeConnection();
+		
+		String sql = "SELECT * FROM product where id = ?;";
+		
+		PreparedStatement preStmt = connection.prepareStatement(sql);
+		preStmt.setInt(1, productId);
+								
+		ResultSet resultSet = preStmt.executeQuery();
+
+		Product product = new Product();
+		
+		if (resultSet.next()) {
+			product.setId(resultSet.getInt("id"));
+			product.setName(resultSet.getString("name"));
+			product.setPrice(resultSet.getInt("price"));
+			product.setImgName(resultSet.getString("img_name"));
+			product.setIs_new(resultSet.getBoolean("is_new"));
+			product.setQuantity(resultSet.getInt("quantity"));
+			product.setDescription(resultSet.getString("description"));		
+			
+			} else {
+			return null;
+		}
+		
+		return product;
+	}
+	
+}
---
+++ src/main/java/sql/connection/DBConnection.java
@@ -0,0 +1,19 @@
+package sql.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	public static Connection makeConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/be5-final-project?serverTimezone=UTC", 
+					"thaolo",
+					"thao123!");
+			return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
